704088ab738f5360f583dfc20dc624a31a478679
Druc Anastasia <druc.aanastasiaa@gmail.com>
2026-05-01 21:50:09 +0300

    builder

---
+++ Patterns/Builder.cs
@@ -0,0 +1,23 @@
+﻿using Task_Management.Models;
+namespace Task_Management.Patterns
+{
+    public class Builder
+    {
+        private TaskModel _task = new TaskModel();
+        public Builder SetTitle(string title)
+        {
+            _task.Title = title;
+            return this;
+        }
+        public Builder SetDate(DateTime date)
+        {
+            _task.Date = date;
+            return this;
+        }
+        public Builder SetType(string type)
+        {
+            _task.Type = type;
+            return this;
+        }
+    }
+}
